1249d7626d42b5a693d4a3a1f0ba504442fb8bb1
gounthar <gounthar@gmail.com>
2024-01-31 17:57:52 +0100

    The send_request function sends a POST request to the /light endpoint of the server with a JSON body containing the board name and color. Please note that you need to install the requests library using pip: ./bin/pip install requests You can then run this script to send a request to the server. The server should light up the LEDs associated with the board name in the specified color.

---
+++ examples/light-client.py
@@ -0,0 +1,23 @@
+# In this script, replace 'http://your_server_ip_address:5000/light' with the actual IP address and port of your server.
+# The send_request function sends a POST request to the /light endpoint of the server with a JSON body containing the board name and color.
+# Please note that you need to install the requests library using pip:
+# ./bin/pip install requests
+# You can then run this script to send a request to the server. The server should light up the LEDs associated with the board name in the specified color.
+
+import requests
+import socket
+
+def send_request(board_name, color):
+    url = 'http://goun-3bplus-1.local:5000/light'
+    data = {'board_name': board_name, 'color': color}
+    response = requests.post(url, json=data)
+    print(f'Response from server: {response.text}')
+
+if __name__ == '__main__':
+    # Get the hostname
+    board_name = socket.gethostname()
+
+    # Define the color
+    color = [255, 0, 0]  # Red
+
+    send_request(board_name, color)
