96ccfd4bc7f269bb352198f56fb35651c3f853d5
gounthar <gounthar@gmail.com>
2024-02-16 17:47:13 +0100

    All IPs where ssh is open are now seen.

--- find-new-name.sh
+++ find-new-name.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set +x
 
 # Default values
@@ -15,18 +15,23 @@ fi
 owner=$(echo $owner | cut -c 1-4)
 
 find_ssh_machines() {
-    nmap -p 22 192.168.1.0/24 -oG - | awk '/22\/open/{print $2}'
+    nmap -p 22 192.168.1.0/24 -oG - | awk '/Host:/{ip=$2} /22\/open|22\/filtered/{print ip}'
 }
 
 # Get the hostnames and IP addresses of all machines in the local network
 # The awk command is used to extract the hostname and IP address from the output of the avahi-browse command
 hostnames_and_ips=$(avahi-browse -alr | awk '/=/{print $4, $8}')
 
+# Print the value of hostnames_and_ips for debugging
+echo "hostnames_and_ips: $hostnames_and_ips"
+
 for hostname_and_ip in $hostnames_and_ips; do
     # Use awk to separate the hostname and IP address
     hostname=$(echo $hostname_and_ip | awk '{print $1}')
     ip=$(echo $hostname_and_ip | awk '{print $2}')
-    echo "IP: $ip, Hostname: $hostname"
+
+    # Print the values of hostname and ip for debugging
+    echo "hostname: $hostname, ip: $ip"
 done
 
 # Generate the new name
