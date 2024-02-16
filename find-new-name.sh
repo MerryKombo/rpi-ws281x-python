4b397b98a101edf2d2d55cf2628237f8386daf64
gounthar <gounthar@gmail.com>
2024-02-16 17:51:12 +0100

    Use avahi-resolve to associate a name to the IP.

--- find-new-name.sh
+++ find-new-name.sh
@@ -18,17 +18,15 @@ find_ssh_machines() {
     nmap -p 22 192.168.1.0/24 -oG - | awk '/Host:/{ip=$2} /22\/open|22\/filtered/{print ip}'
 }
 
-# Get the hostnames and IP addresses of all machines in the local network
-# The awk command is used to extract the hostname and IP address from the output of the avahi-browse command
-hostnames_and_ips=$(avahi-browse -alr | awk '/=/{print $4, $8}')
+# Get the IP addresses of all machines with open or filtered SSH ports
+ssh_machines=$(find_ssh_machines)
 
-# Print the value of hostnames_and_ips for debugging
-echo "hostnames_and_ips: $hostnames_and_ips"
+# Print the value of ssh_machines for debugging
+echo "SSH machines: $ssh_machines"
 
-for hostname_and_ip in $hostnames_and_ips; do
-    # Use awk to separate the hostname and IP address
-    hostname=$(echo $hostname_and_ip | awk '{print $1}')
-    ip=$(echo $hostname_and_ip | awk '{print $2}')
+for ip in $ssh_machines; do
+    # Use avahi-resolve to get the hostname for the IP address
+    hostname=$(avahi-resolve -4 -a $ip | awk '{print $2}')
 
     # Print the values of hostname and ip for debugging
     echo "hostname: $hostname, ip: $ip"
